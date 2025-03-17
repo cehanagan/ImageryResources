edd7dab91466239d18bcdef5e53842d4a8621907
cehanagan <74363782+cehanagan@users.noreply.github.com>
2025-03-17 06:47:34 -0600

    Fix parallelDisp in TiffTools.py
    sin/cos mix

--- Functions/TiffTools.py
+++ Functions/TiffTools.py
@@ -461,9 +461,9 @@ def projectDisp(ewtif,nstif,azimuth,mask=None,partif='ParallelDisp.tif',perptif=
 
     # Rotation from en (xy) to fault parallel and perp, must convert azimuth
     theta = (90-azimuth)*np.pi/180
-    par = ns*np.sin(theta)+ew*np.cos(theta)
+    par = ns*np.cos(theta)+ew*np.sin(theta)
     par[~nodata_mask] = nodata
-    perp = -1*ns*np.cos(theta)+ew*np.sin(theta)
+    perp = -1*ns*np.sin(theta)+ew*np.cos(theta)
     perp[~nodata_mask] = nodata
 
     save_geotiff(par,partif, ewds.GetGeoTransform(), ewds.GetProjection(),nodata=nodata)
